0e382c82a5fa40450f8b6cbb7cdf81ce44cf04d5
Amir Hamza <amirhamza27940@gmail.com>
2025-11-06 22:29:09 +0600

    some addition

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,7 +2,7 @@ import { Testimonials } from '@/components/Testimonials';
 import { Projects } from '@/components/Projects';
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
-import Navbar from './Navbar';
+
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
@@ -11,7 +11,6 @@ import Contact from './Contact';
 
 export {
   Hero,
-  Navbar,
   About,
   Tech,
   Experience,
